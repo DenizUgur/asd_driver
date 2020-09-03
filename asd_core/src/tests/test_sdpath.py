ce89e96f7812f74bcbb662f917be93b7eb042947
DenizUgur <deniz343@gmail.com>
2020-09-03 19:05:24 +0300

    unittest

---
+++ asd_core/src/tests/test_sdpath.py
@@ -0,0 +1,18 @@
+import unittest
+import numpy as np
+
+from sdpath.lcp import LCP
+
+
+class TestLCP(unittest.TestCase):
+    # * Tests, R connection
+    def test_calculation(self):
+        self.lcp = LCP()
+        self.lcp.process_dem(np.zeros((50, 50)))
+        self.lcp.set_points((0, 0), (30, 30))
+        path, _ = self.lcp.calculate()
+        self.assertIsInstance(path, np.ndarray)
+
+
+if __name__ == "__main__":
+    unittest.main()
