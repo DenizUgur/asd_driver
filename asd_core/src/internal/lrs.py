d707d7628df02534ffd23c7ab1d1be588799cca3
DenizUgur <deniz343@gmail.com>
2020-09-09 01:25:11 +0300

    LRS now works for scale too

--- asd_core/src/internal/lrs.py
+++ asd_core/src/internal/lrs.py
@@ -3,10 +3,13 @@ import pickle
 import os
 import math
 import yaml
+import sympy as sp
 import numpy as np
+from scipy.optimize import minimize
 from threading import Thread
 
 from ar_track_alvar_msgs.msg import AlvarMarkers
+from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseWithCovarianceStamped
 
 
@@ -62,10 +65,10 @@ class Marker:
 
 
 class LRS:
-    LIMIT = 50
+    LIMIT = 20
 
     def __init__(self, driver, clean=False):
-        self.config = yaml.load(open("./config.yaml"), Loader=yaml.FullLoader)
+        self.config = yaml.load(open("./config/config.yaml"), Loader=yaml.FullLoader)
         self.driver_instance = driver
         self.visible_landmarks = 0
         self.pose = None
@@ -74,13 +77,16 @@ class LRS:
         self.M_TRANS = None
 
         if (
-            os.path.exists("/tmp/LRS_matrix.npy")
+            os.path.exists("/tmp/LRS_matrix.bak")
             and os.path.exists("/tmp/LRS_landmarks.bak")
             and not clean
         ):
             self.recovered = True
-            self.M_TRANS = np.load("/tmp/LRS_matrix.npy")
-            self.driver_instance.M_T = np.median(self.M_TRANS, axis=2)
+            bakfile = open("/tmp/LRS_matrix.bak", "rb")
+            self.M_TRANS = pickle.load(bakfile)
+            self.update_MT()
+            bakfile.close()
+
             bakfile = open("/tmp/LRS_landmarks.bak", "rb")
             self.dictionary = pickle.load(bakfile)
             bakfile.close()
@@ -92,7 +98,9 @@ class LRS:
                     self.config["initial_position"]["y"],
                 ),
             )
-            landmarks = yaml.load(open("./landmarks.yaml"), Loader=yaml.FullLoader)
+            landmarks = yaml.load(
+                open("./config/landmarks.yaml"), Loader=yaml.FullLoader
+            )
             for val in landmarks["landmarks"]:
                 m_id, x, y = val.values()
                 self.dictionary[m_id] = Marker(m_id, (x, y))
@@ -110,7 +118,9 @@ class LRS:
         try:
             while not rospy.core.is_shutdown():
                 try:
-                    np.save("/tmp/LRS_matrix", self.M_TRANS)
+                    bakfile = open("/tmp/LRS_matrix.bak", "wb")
+                    pickle.dump(self.M_TRANS, bakfile)
+                    bakfile.close()
 
                     bakfile = open("/tmp/LRS_landmarks.bak", "wb")
                     pickle.dump(self.dictionary, bakfile)
@@ -130,25 +140,10 @@ class LRS:
                 "/ar_pose_marker", AlvarMarkers, lambda p: self.commit(p.markers),
             )
             rospy.Subscriber(
-                self.config["pose_with_covariance_topic"],
-                PoseWithCovarianceStamped,
-                self.update_pose,
+                self.config["odometry_topic"], Odometry, self.update_pose,
             )
 
-            rospy.wait_for_message("/ar_pose_marker", AlvarMarkers)
-            rospy.wait_for_message(
-                self.config["pose_with_covariance_topic"], PoseWithCovarianceStamped
-            )
-
-            rospy.loginfo("Landmark Recognition System has started the worker thread")
-
-            M_tr = lambda a, dx, dy: np.array(
-                [
-                    [math.cos(a), -math.sin(a), 0],
-                    [math.sin(a), math.cos(a), 0],
-                    [dx, dy, 1],
-                ]
-            )
+            rospy.wait_for_message(self.config["odometry_topic"], Odometry)
 
             # Save starting point and rcp pose for that
             if not self.recovered:
@@ -158,12 +153,15 @@ class LRS:
             else:
                 rospy.loginfo("Recovered from backup file.")
 
+            rospy.wait_for_message("/ar_pose_marker", AlvarMarkers)
+
             while not rospy.core.is_shutdown():
                 try:
                     if self.has_landmarks():
                         px, py = self.pose
                         for m_id, m in self.dictionary.items():
-                            a, x, y = m.loc()
+                            a, _, _ = m.loc()
+                            x, y = m.kloc()
                             if a > 0:
                                 m.update_distance((px, py))
                             if m_id != -1 and a == 1 and not m.logged:
@@ -174,6 +172,7 @@ class LRS:
 
                         # * Calculate Transformation Matrix
                         if self.visible_landmarks > 0:
+                            # Sort landmarks
                             landmarks = [v for k, v in self.dictionary.items()]
                             landmarks = sorted(
                                 landmarks,
@@ -185,64 +184,153 @@ class LRS:
                                 reverse=True,
                             )
 
-                            p1_m, p2_m = landmarks[:2]
-                            if (
-                                len(p1_m.observed_positions) == 0
-                                or len(p2_m.observed_positions) == 0
-                            ):
+                            # Remove the ones where there is no observed position
+                            landmarks = [
+                                l for l in landmarks if len(l.observed_positions) > 0
+                            ]
+                            if len(landmarks) < 2:
                                 continue
 
-                            p1_fr0, p2_fr0 = p1_m.loc(True), p2_m.loc(True)
-                            p1_fr1, p2_fr1 = p1_m.kloc(True), p2_m.kloc(True)
+                            # Get best two landmarks
+                            p1_m, p2_m = landmarks[:2]
 
-                            # theta_to_be_added
-                            ttba = -self.theta_df(
-                                math.atan2(
-                                    p2_fr1[1] - p1_fr1[1], p2_fr1[0] - p1_fr1[0]
-                                ),
-                                math.atan2(
-                                    p2_fr0[1] - p1_fr0[1], p2_fr0[0] - p1_fr0[0]
-                                ),
-                            )
-                            pivot_trans = np.dot(p1_fr0, M_tr(ttba, 0, 0))
-                            M_trans = M_tr(
-                                ttba,
-                                p1_fr1[0] - pivot_trans[0],
-                                p1_fr1[1] - pivot_trans[1],
-                            )
+                            p1_fr0, p1_fr1 = p1_m.loc(True), p1_m.kloc(True)
+                            p2_fr0, p2_fr1 = p2_m.loc(True), p2_m.kloc(True)
 
-                            if self.M_TRANS is None:
-                                self.M_TRANS = M_trans
-                                self.driver_instance.M_T = self.M_TRANS
-                            else:
-                                try:
-                                    _, _, d = self.M_TRANS.shape
-                                    if d == self.LIMIT:
-                                        rospy.loginfo_once(
-                                            "Transformation Matrix is established!"
-                                        )
-                                        m = np.abs(M_trans - np.median(self.M_TRANS, axis=2))
-                                        if 1 > np.sum(m) > -1:
-                                            self.M_TRANS = np.dstack(
-                                                (self.M_TRANS[:, :, 1:], M_trans)
-                                            )
-                                    else:
-                                        self.M_TRANS = np.dstack(
-                                            (self.M_TRANS, M_trans)
-                                        )
-                                except ValueError:
-                                    self.M_TRANS = np.dstack((self.M_TRANS, M_trans))
-
-                                self.driver_instance.M_T = np.median(
-                                    self.M_TRANS, axis=2
+                            # If we have only 2 landmarks then create 2 new points between these two landmarks
+                            if len(landmarks) == 2:
+                                md1_fr0 = np.array(
+                                    [
+                                        p1_fr0[0] + (1 / 3) * (p2_fr0[0] - p1_fr0[0]),
+                                        p1_fr0[1] + (1 / 3) * (p2_fr0[1] - p1_fr0[1]),
+                                        1,
+                                    ]
+                                )
+                                md1_fr1 = np.array(
+                                    [
+                                        p1_fr1[0] + (1 / 3) * (p2_fr1[0] - p1_fr1[0]),
+                                        p1_fr1[1] + (1 / 3) * (p2_fr1[1] - p1_fr1[1]),
+                                        1,
+                                    ]
                                 )
 
-                    rospy.rostime.wallsleep(0.1)
+                                md2_fr0 = np.array(
+                                    [
+                                        p1_fr0[0] + (2 / 3) * (p2_fr0[0] - p1_fr0[0]),
+                                        p1_fr0[1] + (2 / 3) * (p2_fr0[1] - p1_fr0[1]),
+                                        1,
+                                    ]
+                                )
+                                md2_fr1 = np.array(
+                                    [
+                                        p1_fr1[0] + (2 / 3) * (p2_fr1[0] - p1_fr1[0]),
+                                        p1_fr1[1] + (2 / 3) * (p2_fr1[1] - p1_fr1[1]),
+                                        1,
+                                    ]
+                                )
+                            elif len(landmarks) == 3:
+                                # If there is 3 landmarks then create a midpoint between two best landmarks
+                                md1_m = landmarks[2]
+                                md1_fr0, md1_fr1 = md1_m.loc(True), md1_m.kloc(True)
+
+                                md2_fr0 = np.array(
+                                    [
+                                        p1_fr0[0] + (1 / 2) * (p2_fr0[0] - p1_fr0[0]),
+                                        p1_fr0[1] + (1 / 2) * (p2_fr0[1] - p1_fr0[1]),
+                                        1,
+                                    ]
+                                )
+                                md2_fr1 = np.array(
+                                    [
+                                        p1_fr1[0] + (1 / 2) * (p2_fr1[0] - p1_fr1[0]),
+                                        p1_fr1[1] + (1 / 2) * (p2_fr1[1] - p1_fr1[1]),
+                                        1,
+                                    ]
+                                )
+                            else:
+                                md1_m = landmarks[2]
+                                md1_fr0, md1_fr1 = md1_m.loc(True), md1_m.kloc(True)
+                                md2_m = landmarks[3]
+                                md2_fr0, md2_fr1 = md2_m.loc(True), md2_m.kloc(True)
+
+                            sp_x, sp_y, sp_t, sp_s = sp.symbols("sp_x sp_y sp_t sp_s")
+                            sp_M_trans = sp.Matrix(
+                                (
+                                    (sp_s * sp.cos(sp_t), -sp.sin(sp_t), 0),
+                                    (sp.sin(sp_t), sp_s * sp.cos(sp_t), 0),
+                                    (sp_x, sp_y, 1),
+                                )
+                            )
+
+                            sp_eq0 = sp.Matrix(1, 3, p1_fr0) * sp_M_trans
+                            sp_eq1 = sp.Matrix(1, 3, p2_fr0) * sp_M_trans
+                            sp_eq2 = sp.Matrix(1, 3, md1_fr0) * sp_M_trans
+                            sp_eq3 = sp.Matrix(1, 3, md2_fr0) * sp_M_trans
+
+                            sp_A = sp.Matrix((sp_eq0, sp_eq1, sp_eq2, sp_eq3))
+                            b_np = np.array((p1_fr1, p2_fr1, md1_fr1, md2_fr1))
+
+                            delta = lambda result: np.sum(
+                                np.abs(np.array(result) - b_np)
+                            )
+                            f = lambda args: delta(
+                                sp_A.evalf(
+                                    subs={
+                                        sp_x: args[0],
+                                        sp_y: args[1],
+                                        sp_t: args[2],
+                                        sp_s: args[3],
+                                    }
+                                )
+                            )
+                            result = minimize(
+                                f, [0, 0, 0, 1], method="L-BFGS-B", tol=1e-5
+                            )
+
+                            if result.success:
+                                self.update_MT(
+                                    np.array(
+                                        [
+                                            [
+                                                result.x[3] * math.cos(result.x[2]),
+                                                -math.sin(result.x[2]),
+                                                0,
+                                            ],
+                                            [
+                                                math.sin(result.x[2]),
+                                                result.x[3] * math.cos(result.x[2]),
+                                                0,
+                                            ],
+                                            [result.x[0], result.x[1], 1],
+                                        ]
+                                    ),
+                                    f(result.x),
+                                )
+
+                    rospy.rostime.wallsleep(1.5)
                 except Exception as why:
                     rospy.logerr("Error in LRS... Recovering...")
                     rospy.logerr(repr(why))
         except Exception as why:
             rospy.logfatal("Landmark Recognition System halted!")
+
+    def update_MT(self, MT=None, error=None):
+        if not MT is None:
+            if self.M_TRANS is None:
+                self.M_TRANS = [(error, MT)]
+            elif len(self.M_TRANS) < self.LIMIT:
+                self.M_TRANS.append((error, MT))
+            else:
+                self.M_TRANS.sort(key=lambda x: x[0], reverse=True)
+                if error < self.M_TRANS[0][0]:
+                    self.M_TRANS.pop(0)
+                    self.M_TRANS.append((error, MT))
+
+        # errors = [e for e, _ in self.M_TRANS]
+        # best = list(filter(lambda v: v[0] <= np.percentile(errors, 25), self.M_TRANS))
+        best = sorted(self.M_TRANS, key=lambda x: x[0])
+        self.driver_instance.M_T = best[0][1]
+        self.driver_instance.M_T_err = best[0][0]
 
     def theta_df(self, unit1, unit2):
         unit1 = math.degrees(unit1)
