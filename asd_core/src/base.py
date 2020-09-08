539de39168d076de99bbacd0b6931a73831f94c9
DenizUgur <deniz343@gmail.com>
2020-09-09 01:25:55 +0300

    refactor and kick start

--- asd_core/src/base.py
+++ asd_core/src/base.py
@@ -13,10 +13,11 @@ import concurrent.futures
 from internal.sector_service import SectorService
 from internal.driver import Driver, DriverStatus
 from internal.lrs import LRS
+from internal.prompt_handler import PromptHandler
 from sdpath.lcp import LCP
 
 # Import types
-from geometry_msgs.msg import PoseWithCovarianceStamped
+from nav_msgs.msg import Odometry
 
 
 def route(payload):
@@ -24,9 +25,19 @@ def route(payload):
     Driver_Instance.update_pose(payload.pose)
 
 
+prev_cancel = 0
+
+
 def signal_handler(sig, frame):
-    rospy.logerr("User cancelled the task!")
-    Driver_Instance.halt()
+    global prev_cancel
+    if abs(prev_cancel - dt()) < 1:
+        rospy.logfatal("User halted the program!")
+        SectorService_Instance.shutdown()
+        exit(1)
+    else:
+        rospy.logerr("User cancelled the task!")
+        Driver_Instance.halt()
+    prev_cancel = dt()
 
 
 def main(args):
@@ -35,40 +46,52 @@ def main(args):
     # System Initialization
     rospy.init_node("asd_core")
     signal.signal(signal.SIGINT, signal_handler)
-    config = yaml.load(open("./config.yaml"), Loader=yaml.FullLoader)
+    config = yaml.load(open("./config/config.yaml"), Loader=yaml.FullLoader)
 
     # Initialize Modules
     SectorService_Instance = SectorService()
-    Driver_Instance = Driver()
     LCP_Instance = LCP()
+    Driver_Instance = Driver()
     LRS_Instance = LRS(Driver_Instance, clean=args.fresh)
-
-    if not args.dryrun:
-        Driver_Instance.halt()
-        rospy.loginfo("Searching for the first landmark")
-        while not LRS_Instance.has_landmarks() or LRS_Instance.M_TRANS is None:
-            angular_speed = 0.7
-            for _, m in LRS_Instance.dictionary.items():
-                if m.id != -1 and m.visible_flag and m.accuracy() != 1:
-                    angular_speed = 0.1
-                    break
-
-            direction = -1 if args.direction == "right" else 1
-            Driver_Instance.move(dl=0, da=angular_speed * direction)
-            rospy.rostime.wallsleep(0.2)
-        rospy.loginfo("Search completed")
-        Driver_Instance.halt()
+    PH = PromptHandler(Driver_Instance)
 
     # Subscribe to relevant topics and services
-    rospy.Subscriber(config["pose_with_covariance_topic"], PoseWithCovarianceStamped, route)
+    rospy.Subscriber(config["odometry_topic"], Odometry, route)
 
     # Wait for elevation map to be available
     rospy.loginfo("Waiting for topics to become online...")
     rospy.wait_for_service("/elevation_mapping/get_submap", timeout=60)
-    rospy.wait_for_message(
-        config["pose_with_covariance_topic"], PoseWithCovarianceStamped, timeout=10
-    )
+    rospy.wait_for_message(config["odometry_topic"], Odometry, timeout=10)
     rospy.loginfo("OK!")
+
+    if not args.dryrun and args.fresh:
+        Driver_Instance.halt()
+        rospy.loginfo("Searching for the first landmark")
+
+        # Kick start rover
+        rospy.loginfo("Kick-starting the rover by 3 meters on +x axis (relative)")
+        Driver_Instance.go_to_position(3, 0)
+
+        rospy.loginfo("Waiting for maximum of 5 seconds to initialize Transformation Matrix")
+        start = dt()
+        while not LRS_Instance.has_landmarks() or LRS_Instance.M_TRANS is None:
+            if dt() - start > 5:
+                break
+
+        while not LRS_Instance.has_landmarks() or LRS_Instance.M_TRANS is None:
+            rospy.loginfo_once("Rotating around base_link axis to look for landmarks")
+            angular_speed = config["ar_tags"]["search_vel_max"]
+            for _, m in LRS_Instance.dictionary.items():
+                if m.id != -1 and m.visible_flag and m.accuracy() != 1:
+                    angular_speed = config["ar_tags"]["search_vel_min"]
+                    break
+
+            direction = -1 if args.direction == "right" else 1
+            Driver_Instance.move(dl=0, da=angular_speed * direction)
+
+            rospy.rostime.wallsleep(0.05)
+        rospy.loginfo("Search completed")
+        Driver_Instance.halt()
 
     if args.dryrun:
         time.sleep(2)
@@ -93,30 +116,8 @@ def main(args):
                 Driver_Instance.is_target_reached()
                 or Driver_Instance.flag == DriverStatus.HALT
             ):
-                # TODO: Complete redesign (Confirmation, custom stdout etc.)
-                rospy.loginfo(
-                    "Current location: x={:.2f} y={:.2f}".format(
-                        *Driver_Instance.get_current_loc(True)
-                    )
-                )
-                while 1:
-                    try:
-                        print("Actions\n\tNW: New Waypoint\tE: Exit")
-                        prompt = str(input("Write action: "))
-                        if prompt == "E":
-                            prompt = str(
-                                input(
-                                    "Are you sure? This action is irreversible! (write 'exit' to exit) "
-                                )
-                            )
-                            if prompt == "exit":
-                                raise Exception("User decided to exit.")
-
-                        x, y = float(input("X=")), float(input("Y="))
-                        Driver_Instance.flag = DriverStatus.RESUME
-                        break
-                    except ValueError as why:
-                        rospy.logerr(repr(why))
+                x, y = PH.query()
+                Driver_Instance.flag = DriverStatus.RESUME
             else:
                 rospy.loginfo(
                     "{:.1f}% completed".format(Driver_Instance.percent_complete())
@@ -156,7 +157,7 @@ def main(args):
                     rospy.loginfo("Took {:.2f} seconds to ray trace".format(last_time))
                 rospy.loginfo("{:.1f}% successful".format(100 * map_count / (loop)))
             else:
-                rospy.logerr("Error occured on local ap retrieval")
+                rospy.logerr("Error occured on local map retrieval")
                 continue
 
             start = dt()
@@ -200,12 +201,16 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="ASD Driver")
-    parser.add_argument("--fresh", help="Do not use previous backup files", action="store_true")
-    parser.add_argument("--dryrun", help="Do not start the control loop", action="store_true")
+    parser.add_argument(
+        "--fresh", help="Do not use previous backup files", action="store_true"
+    )
+    parser.add_argument(
+        "--dryrun", help="Do not start the control loop", action="store_true"
+    )
     parser.add_argument(
         "--direction",
         help="Choose starting search direction",
         choices=["left", "right"],
-        default="left"
+        default="left",
     )
     main(parser.parse_args())
