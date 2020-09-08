d0ef10c8e3cb2a8ba296e26e480c88290fd3b375
DenizUgur <deniz343@gmail.com>
2020-09-09 01:24:21 +0300

    update

--- start.sh
+++ start.sh
@@ -6,6 +6,12 @@ echo "source /catkin_ws/devel/setup.bash" >> /etc/bash.bashrc
 echo "ROS_IP=${ROS_IP}" >> /etc/environment
 echo "ROS_MASTER_URI=${ROS_MASTER_URI}" >> /etc/environment
 
-python /root/.local/lib/python2.7/site-packages/freedomrobotics/agent.py &
-
-fg %1
+if [ -z "$EXEC_UNIT_TESTS" ]
+then
+    python /root/.local/lib/python2.7/site-packages/freedomrobotics/agent.py &
+    fg %1
+else
+    echo "Starting execution of unit tests..."
+    roscd asd_core/src
+    python3 root_test.py
+fi
